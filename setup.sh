4a2d753194f246fcfde830c9299ede822c6386e5
Krish Suchak <suchak.krish@gmail.com>
2019-09-03 12:48:40 -0400

    again

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ sudo apt-get update
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh -O anaconda.sh
 chmod +x anaconda.sh
 ./anaconda.sh -b
-sudo chown -R travis "$HOME/anaconda3"
 export PATH="$HOME/anaconda3/bin:$PATH"
 hash -r
 conda config --set always_yes yes --set changeps1 no
