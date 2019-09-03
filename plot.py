0b053b5ee0efed194539370ffc90d76ce9cc8407
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2019-09-03 14:19:05 -0400

    Fix Travis CI by reverting Python version (#2)
    * reverting to py3.6
    
    * dev env
    
    * adding secrets
    
    * env var notation
    
    * global vars
    
    * adding display var
    
    * agg backend on travis
    
    * adding anaconda
    
    * fixing conda path
    
    * adding creds file
    
    * activating conda env
    
    * ignore local creds
    
    * fixing env activation
    
    * mini to full conda
    
    * try again
    
    * try again
    
    * env update name
    
    * remove conda_deps
    
    * adding descartes dep
    
    * conda to mini
    
    * readme
    
    * filemodes
    
    * unix line endings
    
    * making miniconda executable
    
    * pls
    
    * fixing setup
    
    * try
    
    * again
    
    * should work
    
    * removing geoip2
    
    * conda to mini
    
    * setup.sh
    
    * pip install
    
    * reverting

--- plot.py
+++ plot.py
@@ -1,10 +1,17 @@
-import numpy as np
+import os
 import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
-from creds import getLogin
 import requests
 import random
+import matplotlib as mpl
+if os.environ.get('DISPLAY', '') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+
+def dev_build():
+    return os.environ['build'] == 'dev'
 
 
 def read_ip_addrs(filename):
@@ -12,9 +19,8 @@ def read_ip_addrs(filename):
         return fp.read().splitlines()
 
 
-def ip2geo(addr):
+def ip2geo(addr, api_key):
     url = 'http://api.ipstack.com/'
-    api_key = getLogin('ipstack')['api_key']
     endpoint = url + addr + '?access_key=' + api_key
 
     response = requests.get(endpoint)
@@ -26,12 +32,12 @@ def ip2geo(addr):
     return geo
 
 
-def get_points(addrs):
+def get_points(addrs, api_key):
     lats = []
     longs = []
 
     for addr in addrs:
-        geo = ip2geo(addr)
+        geo = ip2geo(addr, api_key)
         lats.append(geo['lat'])
         longs.append(geo['long'])
 
@@ -46,19 +52,26 @@ def gpd_plot(points):
     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
     ax = world.plot()
     gdf.plot(ax=ax, marker='*', color='red', markersize=20)
-    plt.savefig('map.png', format='png', dpi=1200)
-    plt.savefig('map.svg', format='svg', dpi=1200)
+    plt.savefig('map.png', format='png', dpi=300)
+    # plt.savefig('map.svg', format='svg', dpi=1200)
     plt.show()
 
 
 def main():
-    filename = 'mock_data1.txt' if random.random() < 0.5 else 'mock_data2.txt'
+    if dev_build():
+        ipstack = os.environ['ipstack']
+    else:
+        from creds import getLogin
+        ipstack = getLogin('ipstack')['api_key']
+
+    filename = 'mock_data.txt'
     print('Reading ip addresses from mock data...')
     addrs = read_ip_addrs(filename)
     print('Done reading.')
+
     print('Shuffling mock data...')
     random.shuffle(addrs)
-    choose = 10
+    choose = 5
     data = addrs[:choose]
     print('Done shuffling.')
 
@@ -66,31 +79,23 @@ def main():
     # geo = ip2geo(ip)
     # print(geo)
 
-    # df = pd.DataFrame({'lat': [geo['lat']], 'long': [geo['long']]})
-    # print(data)
+    geos = get_points(data, ipstack)
 
-    # geos = get_points(data)
-    geos = {
-            'lats': [
-                37.56100082397461, None, 36.078330993652344, 42.50128936767578,
-                41.84885025024414, 13.756329536437988, 35.69628143310547,
-                43.034759521484375, -33.86714172363281, None],
-            'longs': [
-                126.98265075683594, None, 120.3369369506836, -71.06672668457031,
-                -87.67124938964844, 100.50177001953125, 139.73855590820312,
-                -78.50792694091797, 151.2071075439453, None]
-            }
+    # mock data
+    # geos = {
+    #         'lats': [
+    #             37.56100082397461, None, 36.078330993652344, 42.50128936767578,
+    #             41.84885025024414, 13.756329536437988, 35.69628143310547,
+    #             43.034759521484375, -33.86714172363281, None],
+    #         'longs': [
+    #             126.98265075683594, None, 120.3369369506836, -71.06672668457031,
+    #             -87.67124938964844, 100.50177001953125, 139.73855590820312,
+    #             -78.50792694091797, 151.2071075439453, None]
+    #         }
     print('Plotting points...')
     gpd_plot(geos)
     print('Success.')
     return 0
 
+
 main()
-
-# print(read_ip_addrs('mock_data1.txt'))
-
-# print(ip2geo('205.153.95.177'))
-
-# 38.85377883911133
-# -77.04876708984375
-# print(dir(plt))
---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh -O anaconda.sh
+chmod +x anaconda.sh
+./anaconda.sh -b
+export PATH="$HOME/anaconda3/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update conda
+conda info -a
+conda env create -n dev -f environment.yml
+source activate dev
